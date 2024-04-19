6e776804ec75b35990f8f2226d1f98599b03e4cf
Nishan Giri <nishan.giri789@gmail.com>
2024-04-19 23:42:06 +0530

    Added update and delete api

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const Product = require('./models/product.model.js'); 
-const app = express()
-app.use(express.json()); 
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const app = express();
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("Hello from node api server")
+app.get("/", (req, res) => {
+  res.send("Hello from node api server");
 });
 
-app.get('/api/products', async (req,res) => {
-    try {
-        const response = await Product.find({});
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({message: error.message});
+//GET API
+app.get("/api/products", async (req, res) => {
+  try {
+    const response = await Product.find({});
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by id API
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await Product.findById(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Create API
+app.post("/api/products", async (req, res) => {
+  try {
+    const response = await Product.create(req.body);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Put API
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await Product.findByIdAndUpdate(id, req.body);
+    if(!request) {
+        return res.status(404).json({message: 'Product not found'});
     }
+    const response = await Product.findById(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.get('/api/products/:id', async (req,res) => {
-    try {
-        const { id } = req.params;
-        const response = await Product.findById(id);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({message: error.message});
+//Delete API
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await Product.findByIdAndDelete(id, req.body);
+    if(!response) {
+        return res.status(404).json({message: 'Product not found'});
     }
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.post('/api/products', async (req,res) => {
-    try {
-        const response = await Product.create(req.body);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-mongoose.connect("mongodb+srv://admin:HlR4hFj9d8IzCwJ7@backenddb.1y57xnx.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+mongoose
+  .connect(
+    "mongodb+srv://admin:HlR4hFj9d8IzCwJ7@backenddb.1y57xnx.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
   .then(() => {
-    console.log('Connected to DB!');
+    console.log("Connected to DB!");
     app.listen(3000, () => {
-    console.log("Server is running in port 3000") 
-})
-    })
-    .catch(() => {
-        console.log('Not connected');
+      console.log("Server is running in port 3000");
     });
+  })
+  .catch(() => {
+    console.log("Not connected");
+  });
--- models/product.model.js
+++ models/product.model.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Product name cannnot be empty']
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        price: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        image: {
-            type: String,
-            required: false
-        }
+  {
+    name: {
+      type: String,
+      required: [true, "Product name cannnot be empty"],
     },
-    {
-        timestamps: true
-    }
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
 module.exports = Product;
